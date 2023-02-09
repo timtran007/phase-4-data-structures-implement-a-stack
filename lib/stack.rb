5e8746b50af187641fdebea1af8006cb50226934
Tim <timtran007@gmail.com>
2023-02-09 00:21:25 -0600

    commit tree ccb9f3b59bdf04006409f7880d8bc48c41440501

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,47 @@
 # your code here
+class Stack
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        raise "Stack Overflow" if full?
+        @stack.push(value)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        self.size == 0
+    end
+
+    def full?
+        self.size == @limit
+    end
+
+    def search(target)
+        @stack.each_with_index do |value, index|
+            return self.size - index - 1 if value == target
+        end
+        -1
+    end
+
+end
+
+
+
+
+
+
+
